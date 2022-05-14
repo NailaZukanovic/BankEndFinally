698e61a5cb58b7afa88154e099c726514936f5c9
unknown <nailazukanovic@gmail.com>
2022-05-14 14:23:50 +0200

    nesto

---
+++ SuperHeroAPI/Data - Copy/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BankSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Expo> Expos { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+    }
+}
+
